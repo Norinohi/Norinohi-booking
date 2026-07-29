2de6c0f10278ab8e3407b558eb60f1753ae56d9a
mykhailo <m.diakovych13@gmail.com>
2026-07-29 18:20:55 +0300

    feat(ui): tune DestinationCard padding and subtitle size

--- packages/ui/src/components/data-display/card-destination.tsx
+++ packages/ui/src/components/data-display/card-destination.tsx
@@ -25,9 +25,9 @@ function DestinationCard({
       <CardMedia className="aspect-[4/3] rounded-xl">
         <img src={image} alt={imageAlt} />
         <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/10 to-transparent" />
-        <div className="absolute inset-x-0 bottom-0 flex flex-col gap-1 p-5 text-white">
+        <div className="absolute inset-x-0 bottom-0 flex flex-col gap-1.5 p-8 text-white">
           <span className="text-2xl font-semibold">{title}</span>
-          {subtitle && <span className="text-base text-white/85">{subtitle}</span>}
+          {subtitle && <span className="text-xl text-white/85">{subtitle}</span>}
         </div>
       </CardMedia>
     </Card>
